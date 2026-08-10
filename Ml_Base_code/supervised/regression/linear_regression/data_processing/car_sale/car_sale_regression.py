647f5c7de7a5d26ac82be549bdf85e839265420c
Nguyen Khoi <nguyenhongkhoikm@gmail.com>
2026-08-10 12:41:57 +0700

    refactor

--- Ml_Base_code/supervised/regression/linear_regression/data_processing/car_sale/car_sale_regression.py
+++ Ml_Base_code/supervised/regression/linear_regression/data_processing/car_sale/car_sale_regression.py
@@ -27,9 +27,8 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
-# ==========================================
-# 4. HUẤN LUYỆN MÔ HÌNH REGRESSION
-# ==========================================
+#model 
+
 model = LinearRegression()
 model.fit(X_train, y_train)
 
